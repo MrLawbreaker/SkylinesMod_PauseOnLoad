84276595f5904fc793020b616bd3e7da3dc8fe3e
MrLawbreaker <mrlawbreaker.github@gmail.com>
2015-03-16 13:14:45 +0100

    Added functionality

---
+++ PauseOnLoad/PauseOnLoadMod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICities;
+
+namespace PauseOnLoad
+{
+    public class Mod : IUserMod
+    {
+
+        public string Description
+        {
+            get { return "Automatically pauses the game when loaded"; }
+        }
+
+        public string Name
+        {
+            get { return "Pause on load"; }
+        }
+    }
+
+    public class PauseOnLoadMod : LoadingExtensionBase
+    {
+        ILoading loading;
+
+        public override void OnCreated(ILoading loading)
+        {
+            this.loading = loading;
+        }
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            loading.managers.threading.simulationPaused = true;
+        }
+    }
+}
